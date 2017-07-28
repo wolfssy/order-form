b4b9876e4e7f1db83c071282159115f65e35717d
Adam Walls <awalls1@gmail.com>
2017-07-27 17:26:09 -0700

    now fixing localStorage

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // Image constructor
 function FormData() {
@@ -16,40 +16,36 @@ function FormData() {
 }
 
 FormData.all = [];
+var addCart = document.getElementById('addToCart');
 
-// turn this into an object
-
-// new FormData
+var selected = document.getElementById('selected');
 
 //event listener to addToCart
 function addToCart(e){
-  JSON.stringify(FormData.all);
+  e.preventDefault();
+  var name = document.getElementById('name').value;
+  var street = document.getElementById('street').value;
+  var city = document.getElementById('city').value;
+  var state = document.getElementById('state').value;
+  var zip = document.getElementById('zip').value;
+  var phone = document.getElementById('phone').value;
+  var credit = document.getElementById('credit').value;
+  var qnty = document.getElementById('qnty').value;
+  console.log('qnty', qnty);
+  // if(name === ' ' || street === ' ' || city === ' '|| state === ' '|| zip === ' '|| phone === ' '|| credit === ' '|| qnty === ' '){
+  //   alert('Please fill in ALL the fields');
+  //   return;
+  // }
+  // JSON.stringify(FormData.all);
   localStorage.setItem('FormData.all', JSON.stringify(FormData.all));
-  FormData.name = document.getElementById('name');
-  FormData.street = document.getElementById('street');
-  FormData.city = document.getElementById('city');
-  FormData.state = document.getElementById('state');
-  FormData.zip = document.getElementById('zip');
-  FormData.phone = document.getElementById('phone');
-  FormData.credit = document.getElementById('credit');
-  FormData.qnty = document.getElementById('qnty');
-  //
-  // var selected = document.getElementById('selected');;
-  // var name = document.getElementById('name');
-  // var street = document.getElementById('street');
-  // var city = document.getElementById('city');
-  // var state = document.getElementById('state');
-  // var zip = document.getElementById('zip');
-  // var phone = document.getElementById('phone');
-  // var credit = document.getElementById('credit');
-  // var qnty = document.getElementById('qnty');
-  if(name === '' || street === '' || city === ''|| state === ''|| zip === ''|| phone === ''|| credit === ''|| qnty === ''){
-    alert('Please fill in ALL the fields');
-    return;
-  }
-  console.log('blah');
-  new FormData(name,street,city,state,zip,phone,credit,qnty);
-  console.log(FormData.all);
-}
+  // localStorage.setItem('Pics', JSON.stringify(Pictures.all));
+  console.log(localStorage);
 
-document.getElementById('addToCart').addEventListener('click', addToCart);
+  console.log('inside addToCart function');
+  new FormData(name,street,city,state,zip,phone,credit,qnty);
+  // console.log(FormData.all);
+} // addToCart END
+
+
+addCart.addEventListener('click', addToCart);
+// selected.addEventListener('change', ) // ADD A FUNCTION TO FIND IMAGE OF ITEM SELECTED
